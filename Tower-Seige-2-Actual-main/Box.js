4146a311db9a904e9a5d94f8331963d9224a8a66
Aymen117 <65072335+Aymen117@users.noreply.github.com>
2021-01-02 17:48:42 +0000

    Add files via upload

---
+++ Tower-Seige-2-Actual-main/Box.js
@@ -0,0 +1,33 @@
+class Box{
+  constructor(x,y){
+      var options={
+          restitution:0.8,
+          friction:1.0,
+          density:1.0
+      }
+      this.image=loadImage("BlueBoxForProject.png")
+      this.body=Bodies.rectangle(x,y,30,40);
+      World.add(world,this.body);
+      this.visibility=500;
+      
+  }
+  display(){
+      if(this.body.speed<7){
+           this.pos=this.body.position;
+          push();
+          translate(this.pos.x,this.pos.y)
+          rectMode(CENTER);
+          fill("blue")
+          rect(0,0,30,40);
+          pop();
+      }
+      else{
+          World.remove(world,this.body);
+          push();
+          tint(255,this.visibility);
+          this.visibility=this.visibility-5;
+          (this.image, 0,0,this.width,this.height);
+          pop();
+      }
+  }
+}
